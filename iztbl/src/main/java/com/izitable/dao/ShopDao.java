93dbe40a665cf029f887f67939b35b15f9391735
Student <Student@Student-PC>
2023-09-26 04:20:14 +0900

    2023-09-26 SOSO

---
+++ iztbl/src/main/java/com/izitable/dao/ShopDao.java
@@ -0,0 +1,14 @@
+package com.izitable.dao;
+
+import java.util.List;
+
+import com.izitable.model.Shop;
+
+public interface ShopDao {
+
+	List<Shop> list(Shop shop);
+
+	Shop list(int shopCode);
+	
+	
+}
---
+++ iztbl/src/main/java/com/izitable/dao/ShopDaoImpl.java
@@ -0,0 +1,27 @@
+package com.izitable.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.izitable.model.Shop;
+
+@Repository
+public class ShopDaoImpl implements ShopDao {
+
+	@Autowired
+	SqlSession sql;
+	
+	@Override
+	public List<Shop> list(Shop shop) {
+		return sql.selectList("shop.list", shop);
+	}
+
+	@Override
+	public Shop list(int shopCode) {
+		return sql.selectOne("shop.item", shopCode);
+	}
+
+}
